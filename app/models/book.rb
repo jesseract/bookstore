badd9fb28444d186eb70918b9cf41adbf0c7058e
Jesseract <jlpearce@gmail.com>
2015-10-13 10:22:00 -0400

    Fix redundancies

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,6 @@
 class Book < ActiveRecord::Base
   monetize :price_cents
 
-  
-
-  scope :by_published_date, -> { order(published_date: :desc) } 
   scope :filter, -> (search) { where("title LIKE ?", "%#{search}%") } 
  
 end
