b3225547bd0fb6e34e4bf501ed773c96c8a4dea7
Jesseract <jlpearce@gmail.com>
2015-10-14 18:49:08 -0400

    Merge conflicts?

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,9 +3,9 @@ module CurrentCart
 
   private
   def set_cart
-    cart = Cart.find(session[:cart_id])
-  Rescue ActiveRecord::RecordNotFound
-    cart = Cart.create
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
 
     session[:cart_id] = @cart.id
   end
