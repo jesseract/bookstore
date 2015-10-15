515d0f07abd08a9356cc6a36ea44ebd060c04a2d
Jesseract <jlpearce@gmail.com>
2015-10-15 16:48:31 -0400

    Can now edit cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,11 +1,12 @@
-class LineItemsController < InheritedResources::Base
+class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+
+  before_action :set_cart
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   def create
-    book = Book.find(params[:line_item] [:book_id])
-    @line_item = @cart.add_book(book.id)
+    book = Book.find(params[:line_item][:book_id])
+    @line_item = @cart.add_book(book)
 
     respond_to do |format|
       if @line_item.save
@@ -16,7 +17,17 @@ class LineItemsController < InheritedResources::Base
     end
   end
 
+  def destroy
+    @line_item = LineItem.find(params[:id])
+    @line_item.destroy
+    redirect_to edit_cart_path(@cart)
+  end
+
   private
+
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
     def line_item_params
       params.require(:line_item).permit(:book_id, :cart_id)
