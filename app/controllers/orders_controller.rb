dd27b6a73374b74e930870d731c15d6e8bec756b
Jesseract <jlpearce@gmail.com>
2015-10-26 15:24:59 -0400

    Add email functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,7 +35,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-       
+        OrderNotifier.received(@order).deliver       
         format.html { redirect_to books_url, notice: 'Thank you for your order.' }
       else
         render action: 'new'
